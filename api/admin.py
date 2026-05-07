8e19ece9f3ca53bd65aed4da9ffafde0d819b762
wanchaichab <wanchaichab@gmail.com>
2026-05-06 21:48:39 -1000

    update batch process size

--- api/admin.py
+++ api/admin.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter
-from core.data_processing import get_current_batch_id
 from core.tasks import process_current_batch
 
 router = APIRouter()
 
 @router.post("/run-batch")
 async def run_batch():
-    batch_id = get_current_batch_id()
 
-    process_current_batch(batch_id)
+    result = process_current_batch()
 
-    return {"message": f"Batch {batch_id} processed successfully."}
+    return {"message": result}
--- core/tasks.py
+++ core/tasks.py
@@ -3,7 +3,7 @@ import pandas as pd
 from supabase import create_client
 from dotenv import load_dotenv
 from core.messaging import sendInitialMessage
-from core.data_processing import update_lead_status, increment_batch_id
+from core.data_processing import update_lead_status, increment_batch_id, get_current_batch_id
 
 load_dotenv()
 
@@ -11,21 +11,33 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_SECRET")
 processing_batch_size = int(os.environ.get("PROCESSING_BATCH_SIZE", 50))
 supabase = create_client(url, key)
+max_leads = int(os.environ.get("MAX_LEADS"))
 
-def process_current_batch(batch_id):
-    response = (supabase.table("leads")
-        .select("*")
-        .eq("batch_id", batch_id)
-        .eq("status", "pending")
-        .limit(processing_batch_size)
-        .execute()
-    )
-    leads = response.data or []
-    print(f"Processing batch {batch_id} with {len(leads)} records.")
+def process_current_batch():
+    total_processed = 0
+    batches = []
 
-    for lead in leads:
-        sendInitialMessage(lead)
-        update_lead_status(lead["lead_id"], "message_sent")
-        print(f"Sent message to {lead['phone_number']} and updated status of {lead['lead_id']} to message_sent.")
-    print(f"Processed #{len(leads)} leads in batch {batch_id}.")
-    increment_batch_id()
+    while total_processed < max_leads:
+        batch_id = get_current_batch_id()
+        batches.append(batch_id)
+
+        response = (supabase.table("leads")
+            .select("*")
+            .eq("batch_id", batch_id)
+            .eq("status", "pending")
+            .limit(processing_batch_size)
+            .execute()
+        )
+        leads = response.data or []
+        print(f"Processing batch {batch_id} with {len(leads)} records.")
+
+        for lead in leads:
+            sendInitialMessage(lead)
+            update_lead_status(lead["lead_id"], "message_sent")
+            #print(f"Sent message to {lead['phone_number']} and updated status of {lead['lead_id']} to message_sent.")
+        total_processed += len(leads)
+        print(f"Processed #{len(leads)} leads in batch {batch_id}.")
+
+        increment_batch_id()
+    
+    return (f"Processed {total_processed} leads across batches: {batches}")
